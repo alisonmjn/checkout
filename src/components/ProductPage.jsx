6e783d48eb4d1633e15a6ef4a41a36afec03bda3
alisonmbrand <alison.jimenez@brandlive.co>
2024-03-17 20:25:51 -0500

    size error handling, routing and api change management

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,58 +1,60 @@
-import React from 'react'
+import React from 'react';
 import { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import CheckoutModal from './CheckoutModal';
+
+const schema = yup.object({
+  selectedSize: yup.string().required("Please select a size before adding to cart."),
+});
 
 const ProductPage = () => {
   const [product, setProduct] = useState([]);
   const [products, setProducts] = useState([]);
-  const [categoryId, setCategoryId] = useState('');
-  const [selectedColor, setSelectedColor] = useState('');
-  const [selectedSize, setSelectedSize] = useState('');
-  //Modal checkout
   const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const colors = [
-    '#796e72' //Classic gray
-  ];
-
   const sizes = ['S', 'M', 'L', 'XL'];
 
   useEffect(() => {
-    getProduct()
-    getRelatedProducts();
+    getProduct();
   }, []);
 
-  const handleColorChange = (color) => {
-    setSelectedColor(color);
-  };
+  const { register, handleSubmit, formState: { errors }, trigger } = useForm({
+    resolver: yupResolver(schema),
+  });
 
   const handleSizeChange = (size) => {
-    setSelectedSize(size);
+    setProduct((prevState) => ({ ...prevState, selectedSize: size }));
+    trigger("selectedSize");
   };
 
-  const getProduct = async  () => {
+  const getProduct = async () => {
     try {
-        const response = await fetch('https://api.escuelajs.co/api/v1/products/4')
-        const responseData = await response.json()
-        setProduct(responseData);
-        setCategoryId(parseFloat(responseData.category.id))
+      const response = await fetch('https://fakestoreapi.com/products');
+      const responseData = await response.json();
+      const filteredProducts = responseData.filter(product =>
+        product.category === "men's clothing" || product.category === "women's clothing"
+      );
+      const randomIndex = Math.floor(Math.random() * filteredProducts.length);
+      const randomProduct = filteredProducts[randomIndex];
+      setProduct(randomProduct);
+      getRelatedProducts(randomProduct.category);
     } catch (error) {
-        console.error(error)
+      console.error(error);
     }
-  }
+  };
 
-  const getRelatedProducts = async () => {
+  const getRelatedProducts = async (category) => {
     try {
-        const response = await fetch(`https://api.escuelajs.co/api/v1/products/?categoryId=${categoryId}`)
-        const responseData = await response.json()
-        const firstFourProducts = responseData.slice(4, 8);
-        setProducts(firstFourProducts);
+      const response = await fetch(`https://fakestoreapi.com/products/category/${category}`);
+      const responseData = await response.json();
+      const firstFourProducts = responseData.slice(4, 8);
+      setProducts(firstFourProducts);
     } catch (error) {
-        console.error(error)
+      console.error(error);
     }
-  }
+  };
 
-  //Modal checkout
   const openModal = () => {
     setIsModalOpen(true);
   };
@@ -61,82 +63,67 @@ const ProductPage = () => {
     setIsModalOpen(false);
   };
 
+  const onSubmit = (data) => {
+    if (!Object.keys(errors).length) {
+      openModal();
+    }
+  };
+
   return (
     <div>
       <div className='m-5 p-5 border-1 border-solid border-slate-300'>
         {product && product.category &&(
-            <>   
+          <>   
             <div className='flex justify-start mb-10'>
-                {product.category.name} / <span className='font-bold ml-1'>{product.title}</span>
-                </div>
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8'>
-                <div className='grid gap-5'>
-                    <img className='rounded-lg shadow-md' src={product.images[0]} alt="silla" />
-                    <div className='grid grid-cols-3 gap-5'>
-                    {product.images.map((image, index) => (
-                        <img key={index} className='rounded-lg shadow-md' src={image} alt="silla" />
-                    ))}
-                    </div>
-                </div>
-                <div className='text-left flex flex-col gap-4 mb-14'>
-                    <h1 className='text-3xl font-semibold'>{product.title}</h1>
-                    <h4 className='text-4xl font-bold italic'><span className='not-italic'> $</span>{product.price}</h4>
-                    <h3 className='text-lg font-medium'>Color:</h3>
-                    <div className='flex flex-wrap gap-2'>
-                        {colors.map((color, index) => (
-                            <button
-                            key={index}
-                            className={`w-8 h-8 rounded-full border border-gray-300 hover:shadow-xl ${
-                                selectedColor === color ? 'border-2' : ''
-                            }`}
-                            style={{ backgroundColor: color }}
-                            onClick={() => handleColorChange(color)}
-                            ></button>
-                        ))}
-                    </div>
-                    <h3 className='text-lg font-medium'>Size:</h3>
-                    <div className='flex flex-wrap gap-2'>
-                        {sizes.map((size) => (
-                            <button
-                                key={size}
-                                className={`w-12 h-12 border border-gray-300 rounded-full mr-2 mb-2 ${
-                                selectedSize === size ? 'bg-orange-400 text-white' : ''
-                                }`}
-                                onClick={() => handleSizeChange(size)}
-                            >
-                                {size}
-                            </button>
-                        ))}
-                    </div>
-                    <button className='w-full h-11 bg-orange-400 text-white rounded-lg' onClick={openModal}>
-                        Add to Cart
-                    </button>
-                    <CheckoutModal isOpen={isModalOpen} onClose={closeModal} />
-                    <h3 className='text-lg font-medium'>Description:</h3>
-                    <p className='font-light'>{product.description}</p>
-                    <br />
-                    <h3 className='text-lg font-medium'>Specifications::</h3>
-                    <ul className='font-light'>
-                        <li>Material: Soft cotton blend.</li>
-                        <li>Color: Classic gray.</li>
-                    </ul>
-                </div>
-              </div>
-            </>
-        )}
-            <div className='mt-10 mb-10'>
-                <h2 className='text-lg font-medium text-left mb-5'>
-                    Related products::
-                </h2>
-                <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-8'>
-                {products.map((product) => (
-                    <img key={product.id} className='rounded-lg shadow-md hover:shadow-xl' src={product.images[0]} alt={product.title}/>
-                 ))}
-                </div>
+              {product.category} / <span className='font-bold ml-1'>{product.title}</span>
             </div>
-        </div> 
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8'>
+              <div className='grid gap-5'>
+                <img className='rounded-lg shadow-md' src={product.image} alt="silla" />
+              </div>
+              <div className='text-left flex flex-col gap-4 mb-14'>
+                <h1 className='text-3xl font-semibold'>{product.title}</h1>
+                <h4 className='text-4xl font-bold italic'><span className='not-italic'> $</span>{product.price}</h4>
+                <h3 className='text-lg font-medium'>Size:</h3>
+                <div className='flex flex-wrap gap-2'>
+                  {sizes.map((size) => (
+                    <label key={size} className="inline-flex items-center">
+                      <input
+                        type="radio"
+                        value={size}
+                        {...register("selectedSize")}
+                        onChange={() => handleSizeChange(size)}
+                        className="h-6 w-6 border border-gray-300 rounded-full mr-2 mb-2"
+                      />
+                      <span className={`h-6 w-6 rounded-full mr-2 mb-2 flex items-center justify-center ${product.selectedSize === size ? 'bg-orange-400 text-white' : ''}`}>{size}</span>
+                    </label>
+                  ))}
+                </div>
+                <p className='text-red-500'>{errors.selectedSize?.message}</p>
+                <input type="submit" onClick={handleSubmit(onSubmit)} className='w-full h-11 bg-orange-400 text-white rounded-lg' value="Add to Cart" />
+                <CheckoutModal isOpen={isModalOpen} onClose={closeModal} />
+                <h3 className='text-lg font-medium'>Description:</h3>
+                <p className='font-light'>{product.description}</p>
+                <br />
+              </div>
+            </div>
+          </>
+        )}
+        {products.length > 0 && (
+          <div className='mt-10 mb-10'>
+            <h2 className='text-lg font-medium text-left mb-5'>
+              Related products:
+            </h2>
+            <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-8'>
+              {products.map((product) => (
+                <img key={product.id} className='rounded-lg shadow-md hover:shadow-xl' src={product.image} alt={product.title}/>
+              ))}
+            </div>
+          </div>
+        )}
+      </div> 
     </div>
-  )
-}
+  );
+};
 
-export default ProductPage
+export default ProductPage;
